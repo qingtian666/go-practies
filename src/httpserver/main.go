83948eedbca5b9536b4907ac829d696c752e9e48
qingtian <1260855435@qq.com>
2021-10-30 16:18:21 +0800

    project create

---
+++ src/httpserver/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	myHandler := http.HandlerFunc(rootHandle)
+	http.Handle("/", WithLogging(myHandler))
+	http.HandleFunc("/healthz", healthz)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok\n")
+}
+
+func rootHandle(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("version", runtime.Version())
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+}
+
+type StatusRecorder struct {
+	http.ResponseWriter
+	Status int
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		recorder := &StatusRecorder{
+			ResponseWriter: w,
+			Status:         200,
+		}
+		h.ServeHTTP(recorder, r)
+		log.Printf("request status: %d, ipAddress: %s", recorder.Status, ClientPublicIP(r))
+	})
+}
+
+func ClientPublicIP(r *http.Request) string {
+	var ip string
+	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
+		ip = strings.TrimSpace(ip)
+		if ip != "" {
+			return ip
+		}
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
